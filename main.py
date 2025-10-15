d1a407419fedc00553d59b90c62bc960140d25c8
LIOR-YAACOV <LIORBE111110@GMAIL.COM>
2025-10-15 08:48:49 +0300

    Added print from programmer 1

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from Employee import Employee
 
 student = Student("Alex", 25, "English", 2, 90)
 # student.foo()
+print("This is from programmer 1")
 
 employee = Employee("John", 40, "Software Engineer", 45000)
 people = [student, employee]
